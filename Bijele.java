5696bbc042e228d7432a52e9b04ee986a3ccef3a
“ming-00” <e0470728@u.nus.edu>
2020-02-29 00:53:20 +0800

    First commit

---
+++ Bijele.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Bijele {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	
+
+		int[] def = {1, 1, 2, 2, 2, 8};
+
+		for(int i = 0; i < def.length; i++) 
+			System.out.print(def[i] - sc.nextInt() + " ");
+			System.out.println();
+	
+	}}
+
+
+			
---
+++ ReversedBinaryNumbers.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ReversedBinaryNumbers {
+
+	static int converter(int num)  {
+		String binary = Integer.toBinaryString(num);
+		int len = binary.length();
+		String converted = "";
+
+		for (int i = len - 1; i >= 0; i--) {	
+			converted += binary.charAt(i);
+		}
+
+		return Integer.parseInt(converted, 2);
+	}
+			
+
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+
+		System.out.println(converter(num));
+	}
+}
+
